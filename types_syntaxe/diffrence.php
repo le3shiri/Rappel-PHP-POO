80435cc62e84e28a3a96f23d2d1d2423f5c3cc2d
MOHAMED AYMANE ELACHIRI <satayman41@gmail.com>
2025-09-16 12:28:35 +0100

    adding diffrence between ?: and ??

--- types_syntaxe/diffrence.php
+++ types_syntaxe/diffrence.php
@@ -16,3 +16,14 @@ echo $row['views'] ?: 'default'; // Ghadi y-printi 'default' hita '0' falsy
 echo "<br>";
 echo $row['excerpt'] ?: 'default'; // Ghadi y-printi 'default' hita '' falsy
 echo "<br>";
+
+
+/*
+ * Table dial l-farq bin ?? w ?:
+ * 
+ * | Operator | Sho kat-shuf         | Mté kat-akhod default value                     | Exemple                                       |
+ * |----------|---------------------|-----------------------------------------------|-----------------------------------------------|
+ * | ??       | Null wlla undefined | Ila l-value null wlla l-key ma-kaynsh         | $row['views'] ?? 0 → y-rja3 0 ila views null wlla m-f9ud |
+ * | ?:       | Truthy/Falsy        | Ila l-value falsy (null, 0, '', false, etc.)   | $row['views'] ?: 0 → y-rja3 0 ila views falsy (7ta lo kan 0) |
+ * 
+ */
